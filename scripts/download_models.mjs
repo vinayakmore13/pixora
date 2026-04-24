c8200269945d2f370718ec3726dea2a9bbfd0aab
vinayakmore13 <vinayakm083@gmail.com>
2026-04-24 14:06:55 +0530

    commit

--- scripts/download_models.mjs
+++ scripts/download_models.mjs
@@ -22,6 +22,7 @@ const files = [
   'face_landmark_68_model-shard1',
   'face_recognition_model-weights_manifest.json',
   'face_recognition_model-shard1',
+  'face_recognition_model-shard2',
   'face_expression_model-weights_manifest.json',
   'face_expression_model-shard1',
 ];
